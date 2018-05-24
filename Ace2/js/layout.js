70781b113830595e6927222ff6169988f5218855
CPFfirmware <andy900508@gmail.com>
2018-05-24 19:16:25 +0800

    Add files via upload

---
+++ Ace2/js/layout.js
@@ -0,0 +1,98 @@
+
+var Layout = {};
+
+Layout.page = '';
+
+Layout.getStringParamFromUrl = function(name, defaultValue) {
+  var val = location.search.match(new RegExp('[?&]' + name + '=([^&]+)'));
+  return val ? decodeURIComponent(val[1].replace(/\+/g, '%20')) : defaultValue;
+};
+
+Layout.setHtml = function (page, pagename, callback) {
+	var xhr = new XMLHttpRequest();
+
+	/*xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            var html = this.responseText;
+			document.getElementById("main").innerHTML = html;
+			document.getElementById("bannerName").setAttribute("key",pagename);
+			Language.init();
+        }
+    };*/
+	
+	xhr.open("GET", './html/'+page+'.html', false);
+	xhr.send();
+
+	var html = xhr.responseText;
+	document.getElementById("main").innerHTML = html;
+	document.getElementById("bannerName").setAttribute("key",pagename);
+	Language.init();
+
+	callback();
+}
+
+Layout.getHtml = function() {
+	Layout.page = Layout.getStringParamFromUrl('page') ? Layout.getStringParamFromUrl('page') : 'none';
+	
+	if (Layout.page === 'none') 
+	{
+		// 404
+	}
+	else if (Layout.page === 'company') {
+		Layout.setHtml(Layout.page, 'about');
+	}
+	else if (Layout.page === 'news') {
+		Layout.setHtml(Layout.page, 'about');
+	}
+	else if (Layout.page === 'contact') {
+		Layout.setHtml(Layout.page, 'contact');
+	}
+	else if (Layout.page === 'process' || Layout.page === 'quality') {
+		Layout.setHtml(Layout.page, 'process', function() {
+			document.write('<script src="./js/effect.js"></script>');
+		});
+	}
+	else 
+	{
+		Layout.setHtml(Layout.page, 'product', function() {
+			document.write('<script src="./js/product.js"></script>');
+		});
+	}
+	
+}
+
+Layout.getHtml();
+
+
+/*function newsInit() {
+	var newsparam = getStringParamFromUrl("news");
+	console.log(newsparam);
+	if(newsparam === undefined) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.onload = function() {
+			var data = JSON.parse(this.responseText);
+			var content = '';
+			for(var key in data.newsList) {
+				content += '<div class="media">' +
+					'<img class="align-self-center mr-4 ml-6 rounded" src="'+data.newsList[key].newsImg+'" alt="...">' +
+					'<div class="media-body">' +
+					'<span class="media-date">'+data.newsList[key].newsDate+'</span>' +
+					'<div class="media-title mb-3 mt-2"><a href="./news.html?news=1">'+data.newsList[key].newsTitle+'</a></div>' +
+					'<p>'+data.newsList[key].newsContent+'<a href="./news.html?news=1">VIEW MORE></a></p></div></div>' +
+					'<br><p>';
+			}
+			content += '<div class="news-page"> <span><u>1</u></span> <span>2</span> <span>3</span> </div>';
+			document.getElementById("newsContain").innerHTML = content;
+		} 
+
+		xhr.open("GET", './msg/'+localStorage.lang+'.json');
+		xhr.send();
+	}
+}*/
+
+
+
+
+
+
---
+++ Ace2/js/product.js
@@ -0,0 +1,60 @@
+var Product = {};
+
+Product.item_length = 15;
+Product.page_length = 3;
+
+Product.setItem = function(pagenum) {
+
+	var content = '';
+	var p = 1;
+	
+	for (var i = 0; i < Product.item_length; i++) {
+		
+		content += '<li class="list">' +
+				'<a onclick="">' +
+					'<div class="item">' +
+						'<img class="img-fluid" src="./images/product/num1p'+p+'.png" alt="..." />' +
+					'</div>' +
+				'</a></li>';
+		
+		if(i >= 3) {
+			p = 2;
+		}
+		if(i >= 7) {
+			p = 3;
+		}
+		if(i >= 11) {
+			p = 4;
+		}
+		
+	}
+
+	var pagecode = Product.getPage(Product.page_length, pagenum);
+
+	document.getElementById("productul").innerHTML = content;
+	document.getElementById("product_page").innerHTML = pagecode;
+}
+
+Product.getPage = function(totalPage, nowPage) {
+
+	var code = '';
+
+	for(var i = 1; i <= totalPage; i++){
+		if(i == nowPage){
+				code += '<span class="active">'+i+'</span>';
+		}else{
+				code += '<span onclick=javascript:location.href="./layout.html?page='+Layout.page+'&num='+i+'">'+i+'</span>';
+		}
+	}
+
+	var prevPage = new Number(nowPage)-1;
+	var nextPage = new Number(nowPage)+1;
+	var prev = (nowPage == 1) ? '<span style="cursor:default"><</span>' : '<span onclick=javascript:location.href="./layout.html?page='+Layout.page+'&num='+prevPage+'"><</span>';
+	var next = (nowPage == totalPage) ? '<span style="cursor:default">></span>' : '<span onclick=javascript:location.href="./layout.html?page='+Layout.page+'&num='+nextPage+'">></span>';
+
+	return prev + code + next;
+}
+
+var pagenum = Layout.getStringParamFromUrl('num') ? Layout.getStringParamFromUrl('num') : 1;
+
+Product.setItem(pagenum);
